f21657ccb4cfcbf3314370520c2da866ac9de9be
Simone-Lemes <simonelemesacs@gmail.com>
2020-09-16 01:16:53 -0300

    escape

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var express = require("express");
+var fs = require("fs");
+
 var app = express();
+
+app.use(express.json());
 
 app.get("/", function (req, res) {
     res.send("hello world");
 });
 
 app.post("/account", (req, res) => {
+    let params = req.body;
     console.log("post account");
+
+    fs.writeFile("account.json", JSON.stringify(params),err =>{
+        console.log(err);
+    })
+
+    res.send("post account");
 });
 
 
