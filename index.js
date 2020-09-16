30cbb47efbcd3f4343c27d924de46fad3e4e3081
Simone-Lemes <simonelemesacs@gmail.com>
2020-09-16 00:53:47 -0300

    escape

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+    res.send("hello world");
+});
+
+app.post("/account", (req, res) => {
+    console.log("post account");
+});
+
+
+app.listen(3000, function () {
+    console.log("api started");
+});
